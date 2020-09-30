6bb0cbf69e0f7435f0dccb1ecbe7eb2dd559b8b0
Matheus Sanches Marquesini <48459614+MatheuSanches@users.noreply.github.com>
2020-09-30 11:47:05 -0300

    Modificações GalleryPage

--- testeMOB/obj/Debug/netstandard2.0/views/GalleryPage.xaml.g.cs
+++ testeMOB/obj/Debug/netstandard2.0/views/GalleryPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace testeMOB {
     public partial class GalleryPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Image selectedImage;
+        private global::Xamarin.Forms.Grid gridLayout;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(GalleryPage));
-            selectedImage = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "selectedImage");
+            gridLayout = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Grid>(this, "gridLayout");
         }
     }
 }
--- testeMOB/views/GalleryPage.xaml.cs
+++ testeMOB/views/GalleryPage.xaml.cs
@@ -14,43 +14,48 @@ namespace testeMOB
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GalleryPage : ContentPage
     {
-        //Criar um grid e um componente imagem 
-        //https://docs.microsoft.com/pt-br/xamarin/xamarin-forms/user-interface/listview/customizing-cell-appearance
-
+        private List<ImageGallery> ImageList;
         public GalleryPage()
         {
             InitializeComponent();
+            ImageList = new List<ImageGallery>();
+            ImageList.Add(new ImageGallery()
+            {
+                Image = "solo1.png"
+            });
         }
 
-        async void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            //! added using Plugin.Media;
-            await CrossMedia.Current.Initialize();
+        
+        
+        //async void Handle_Clicked(object sender, System.EventArgs e)
+        //{
+        //    //! added using Plugin.Media;
+        //    await CrossMedia.Current.Initialize();
 
-            // if you want to take a picture use this
-            // if(!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
-            // if you want to select from the gallery use this
-            if (!CrossMedia.Current.IsPickPhotoSupported)
-            {
-                await DisplayAlert("Sem suporte", "Seu ceular não suporta esta funcionalidade", "Ok");
-                return;
-            }
+        //    // if you want to take a picture use this
+        //    // if(!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+        //    // if you want to select from the gallery use this
+        //    if (!CrossMedia.Current.IsPickPhotoSupported)
+        //    {
+        //        await DisplayAlert("Sem suporte", "Seu ceular não suporta esta funcionalidade", "Ok");
+        //        return;
+        //    }
 
-            //! added using Plugin.Media.Abstractions;
-            // if you want to take a picture use StoreCameraMediaOptions instead of PickMediaOptions
-            var mediaOptions = new PickMediaOptions()
-            {
-                PhotoSize = PhotoSize.Medium
-            };
-            // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
-            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+        //    //! added using Plugin.Media.Abstractions;
+        //    // if you want to take a picture use StoreCameraMediaOptions instead of PickMediaOptions
+        //    var mediaOptions = new PickMediaOptions()
+        //    {
+        //        PhotoSize = PhotoSize.Medium
+        //    };
+        //    // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
+        //    var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
 
-            if (selectedImage == null)
-            {
-                await DisplayAlert("Error", "Não foi possível pegar a imagem, tente novamante", "Ok");
-                return;
-            }
-            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
-        }
+        //    if (selectedImage == null)
+        //    {
+        //        await DisplayAlert("Error", "Não foi possível pegar a imagem, tente novamante", "Ok");
+        //        return;
+        //    }
+        //    selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        //}
     }
 }
